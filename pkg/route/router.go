31a75b16693eec224757d4279646d6b3b22ad906
chenchen <chen.chenjava@sirun.net>
2024-03-13 14:14:53 +0800

    重构

--- pkg/route/router.go
+++ pkg/route/router.go
@@ -2,16 +2,10 @@ package route
 
 import (
 	"github.com/gorilla/mux"
-	"goblog/routes"
 	"net/http"
 )
 
 var Router *mux.Router
-
-func Initialize() {
-	Router = mux.NewRouter()
-	routes.RegisterWebRoutes(Router)
-}
 
 func Name2URL(routeName string, pairs ...string) string {
 	url, err := Router.Get(routeName).URL(pairs...)
